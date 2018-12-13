09042aa52cdb7b5f284ab1f7ef5b52c5ce64bdb8
sylvain <sylvain@sylvain-UX32A>
2018-12-12 23:56:40 -0500

    Not much

--- SN_Analysis/SN_Plots/SN_QPCR_Plot.py
+++ SN_Analysis/SN_Plots/SN_QPCR_Plot.py
@@ -2,6 +2,14 @@
 Created on Dec 12, 2018
 
 @author: sylvain
+
+see https://anthonydebarros.com/2013/02/05/get-json-from-excel-using-python-xlrd/
+and
+https://www.ibm.com/developerworks/library/wa-django/index.html
+and
+http://codetheory.in/parse-read-excel-files-xls-xlsx-javascript/
+
+the approach 
 '''
 import xlrd
 import numpy
@@ -22,7 +30,7 @@ class SN_QPCR_Plot(object):
         return data
     
     def getGroups(self):
-        itemindex = numpy.where(self.data == 1.0)
+        itemindex = numpy.where(self.data == 7.2)
         print itemindex
         return itemindex
         
